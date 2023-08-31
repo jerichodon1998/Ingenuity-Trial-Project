5bc29ffc596c36e9089b9aa73331736cf6edbe15
markjericho <jerichodon1998@gmail.com>
2023-08-31 16:11:10 +0800

    render recipes to homepage

--- src/routes/homePage.tsx
+++ src/routes/homePage.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import {
+	DocumentData,
+	QuerySnapshot,
+	collection,
+	onSnapshot,
+	orderBy,
+	query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase/firebaseConfig";
+import { RecipeInterface } from "../interfaces/Recipe";
+import { Box } from "@mui/material";
+import ListCard from "../components/listCard";
 
 const HomePage: React.FC = () => {
-	return <h1>HomePage</h1>;
+	const [recipes, setRecipes] = useState<RecipeInterface[]>([]);
+
+	const renderRecipes = (): JSX.Element => {
+		return <ListCard recipes={recipes} />;
+	};
+
+	useEffect(() => {
+		const getAllRecipes = () => {
+			const myQuery = query(
+				collection(db, "recipe"),
+				orderBy("createdAt", "asc")
+			);
+			onSnapshot(myQuery, (snapshot: QuerySnapshot): void => {
+				const newData: RecipeInterface[] = snapshot.docs.map(
+					(doc: DocumentData): RecipeInterface => {
+						return {
+							id: doc.id,
+							title: doc.data().title,
+							createdAt: doc.data().createdAt,
+							ingredients: doc.data().ingredients,
+							instructions: doc.data().instructions,
+						};
+					}
+				);
+				setRecipes(newData);
+			});
+		};
+		getAllRecipes();
+	}, []);
+
+	return (
+		<Box>
+			<h1>HomePage</h1>
+			{renderRecipes()}
+		</Box>
+	);
 };
 
 export default HomePage;
