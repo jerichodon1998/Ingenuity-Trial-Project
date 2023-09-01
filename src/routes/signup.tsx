2d1e2ed32acfe0cd0ba5cd0a40b148ed60110051
markjericho <jerichodon1998@gmail.com>
2023-09-01 11:46:19 +0800

    signup ui

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import { Box, FormControl, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { UserCredentialInterface } from "../interfaces/User";
 
 const Signup: React.FC = () => {
-	return <h1>Signup</h1>;
+	const [userCredential, setUserCredential] = useState<UserCredentialInterface>(
+		{ email: "", password: "" }
+	);
+
+	const onFormChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		console.log(e.currentTarget.name);
+		setUserCredential((prevState) => ({
+			...prevState,
+			[e.currentTarget.name]: e.target.value,
+		}));
+	};
+
+	return (
+		<Box flexGrow={1} padding={{ xs: 10, sm: 10, md: 5, lg: 5 }}>
+			<Grid
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					textAlign: "center",
+				}}
+			>
+				<FormControl
+					sx={{
+						border: "1px solid #000",
+						padding: "20px",
+						maxWidth: "340px",
+						borderRadius: "20px",
+					}}
+				>
+					<Typography gutterBottom variant="h5" component="div">
+						Signup With Email and Password
+					</Typography>
+					<Grid container spacing={2} justifyContent={"center"}>
+						<Grid item>
+							<TextField
+								type="email"
+								placeholder="email"
+								name="email"
+								onChange={onFormChange}
+								value={userCredential.email}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								type="password"
+								placeholder="password"
+								name="password"
+								onChange={onFormChange}
+								value={userCredential.password}
+							/>
+						</Grid>
+					</Grid>
+				</FormControl>
+			</Grid>
+		</Box>
+	);
 };
 
 export default Signup;
