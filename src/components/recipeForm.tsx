fa7ca3832ebdc9446f6b805a8ec96835e1515cb2
markjericho <jerichodon1998@gmail.com>
2023-09-02 14:35:32 +0800

    create reusable recipe form

---
+++ src/components/recipeForm.tsx
@@ -0,0 +1,73 @@
+import { Button, FormGroup, Grid, InputLabel, TextField } from "@mui/material";
+import { EditRecipeInterface } from "../interfaces/Recipe";
+
+interface RecipeFormProps {
+	recipe: EditRecipeInterface;
+	buttonText: string;
+	onFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+	onFormChange: (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => void;
+}
+
+const RecipeForm: React.FC<RecipeFormProps> = ({
+	onFormChange,
+	onFormSubmit,
+	recipe,
+	buttonText,
+}) => {
+	return (
+		<Grid
+			container
+			spacing={1}
+			sx={{
+				textAlign: "center",
+				alignContent: "center",
+				justifyContent: "center",
+			}}
+		>
+			<form onSubmit={onFormSubmit}>
+				<FormGroup>
+					<Grid item>
+						<InputLabel>Title</InputLabel>
+						<TextField
+							name="title"
+							value={recipe?.title}
+							onChange={onFormChange}
+						/>
+						<InputLabel>Ingredients</InputLabel>
+						<TextField
+							name="ingredients"
+							multiline
+							maxRows={10}
+							value={recipe?.ingredients}
+							sx={{ width: "400px" }}
+							onChange={onFormChange}
+						/>
+						<InputLabel>Instructions</InputLabel>
+						<TextField
+							name="instructions"
+							multiline
+							maxRows={10}
+							sx={{ width: "400px" }}
+							value={recipe?.instructions}
+							onChange={onFormChange}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Button
+							type="submit"
+							size="large"
+							color="success"
+							variant="contained"
+						>
+							{buttonText}
+						</Button>
+					</Grid>
+				</FormGroup>
+			</form>
+		</Grid>
+	);
+};
+
+export default RecipeForm;
